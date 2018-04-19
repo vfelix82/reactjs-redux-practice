83d2f43c02ff654ff16e776ba8595b4cf1d064e4
Victor Felix <vfelix82@gmail.com>
2018-04-19 11:08:59 -0700

    add working book library webpage to understand redux.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Drsk Tower' },
-    { title: 'Eloquent Ruby' }
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 39 },
+    { title: 'The Drsk Tower', pages: 85 },
+    { title: 'Eloquent Ruby', pages: 1 }
   ]
 }
