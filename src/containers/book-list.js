76b9e2d129d5efd205028d7bd3c86297b73dfde1
Victor Felix <vfelix82@gmail.com>
2018-04-19 09:55:23 -0700

    add working connection between onclick booklist to action function selectbook.

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+        key={book.title}
+        onClick={() => this.props.selectBook(book)}
+        className="list-group-item">{book.title}</li>
       );
     });
   }
@@ -21,6 +26,8 @@ class BookList extends Component {
   }
 }
 
+// Anything returned from this function will end up as props on
+// bookList container
 function mapStateToProps(state) {
   // Whatever is returend will show up as mapStateToProps
   // inside of BookList
@@ -29,4 +36,13 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed to all of our
+  // reducers.
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+// Promote BookList from a component to a container - it needs to know
+// about this new dispatch method, selectBook. Make it available
+// as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
